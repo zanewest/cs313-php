7f8f7f764a369f02944913d5cedb2722fcdb5a15
Zane West <wes14013@byui.edu>
2017-10-15 20:23:33 +0200

    updated index for heroku connection

--- web/week05/index.php
+++ web/week05/index.php
@@ -1,17 +1,33 @@
-
 <?php
-//connect to db
-try
-  {
-  $user = 'postgres';
-  $password = '141220Aa';
-  $db = new PDO('pgsql:host=127.0.0.1;dbname=pureaddictiondb', $user, $password);
-} catch (PDOException $ex)
-  {
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
 
+function get_db() {
+	$db = NULL;
+	try {
+		// default Heroku Postgres configuration URL
+		$dbUrl = getenv('DATABASE_URL');
+		if (!isset($dbUrl) || empty($dbUrl)) {
+			$dbUrl = "postgres://postgres:mniDnb16@localhost:5432/postgres";
+		}
+		// Get the various parts of the DB Connection from the URL
+		$dbopts = parse_url($dbUrl);
+		$dbHost = $dbopts["ec2-50-19-105-113.compute-1.amazonaws.com"];
+		$dbPort = $dbopts["5432"];
+		$dbUser = $dbopts["gymwblcnmjdaqd"];
+		$dbPassword = $dbopts["3802346cf03391ca22c0e54f3b73cb317263ed98acebcdc9d92c36f014be32bc"];
+		$dbName = ltrim($dbopts["path"],'/');
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex) {
+		// If this were in production, you would not want to echo
+		// the details of the exception.
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
+	return $db;
+}
 ?>
 
 <!DOCTYPE html>
